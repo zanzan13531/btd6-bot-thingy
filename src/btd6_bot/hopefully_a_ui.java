f759134dd7de146f61d350b3c76566e8529fae76
zanzan13531 <zanzan13531@gmail.com>
2019-04-16 08:48:14 -0700

    started working on ui, incomplete

---
+++ src/btd6_bot/hopefully_a_ui.java
@@ -0,0 +1,29 @@
+package btd6_bot;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField; 
+
+public class hopefully_a_ui {
+	
+	public static void main(String s[]) {
+		
+		JFrame f = new JFrame("hopefully this works");
+		
+		f.setSize(400, 300);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel p = new JPanel();
+		
+		f.add(p);
+		
+		
+		
+		
+		f.setVisible(true);
+		
+	}
+	
+}
